427f2216f8e5b194fd8821009a1a7d03c3644dca
Yngdlab <145421571+Yngdlab@users.noreply.github.com>
2023-09-19 15:31:53 +0900

    Update グラフテスト.py

--- グラフテスト.py
+++ グラフテスト.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import random
-import openpyxl
-import matplotlib.pyplot as plt  # matplotlibをインポート
+
 import os
 
 # ポート番号の選択肢
@@ -17,46 +16,6 @@ st.write("選択されたポート番号:", selected_port)
 st.title("グラフ表示のテスト")
 st.write("ここにコンテンツを追加します。")
 
-# Excelファイルを開く
-excel_file_path = "C:\\Users\\yhbda\\マイドライブ\\M1＆M2\\研究関連\\VSCode\\サンプルデータ.xlsx"
-df = pd.read_excel(excel_file_path)
-
-# 1列目のデータをリストに変換
-column1_data = df.iloc[:, 0].tolist()
-
-# 1列目のデータがリストに格納されます
-print(column1_data)
-# 2列目のデータをリストに変換
-column2_data = df.iloc[:, 1].tolist()
-
-# 2列目のデータがリストに格納されます
-print(column2_data)
-
-# データをDataFrameに変換
-data = pd.DataFrame({'x': column1_data, 'y': column2_data})
-
-# グラフを表示
-fig, ax = plt.subplots()
-ax.plot(data['x'], data['y'], label='データ')  # データをプロットし、ラベルを設定
-
-# ラベルを追加
-ax.set_xlabel('wavelength (nm)')
-ax.set_ylabel('absorbance (-)')
-
-# サイドバーウィジェット
-st.sidebar.title("サイドバー")
-st.sidebar.button("サイドバーボタン")
-
-# カラムレイアウトを作成
-col1, col2 = st.columns(2)
-
-# カラム1にウィジェットを配置
-with col1:
-    st.button("左側のボタン")
-
-# カラム2にウィジェットを配置
-with col2:
-    st.button("右側のボタン")
 
 # ランダムな16進数を生成
 def generate_random_hex_list(length):
@@ -90,23 +49,3 @@ if st.button("過去の結果"):
     # グラフの表示
     st.pyplot(fig)
 
-# ダウンロードファイル名の入力ウィジェットを表示
-download_filename = st.text_input("ダウンロードファイル名を入力してください (拡張子を含む):")
-download_filename = f"{download_filename}.xlsx"
-
-if st.button("データをエクセルファイルにエクスポート"):
-    if data is not None:
-        if download_filename:
-            # ファイルを指定したファイル名でエクセルファイルに書き込む
-            data.to_excel(download_filename, index=False)
-            
-            # ファイルをダウンロードディレクトリに移動
-            download_dir = os.path.expanduser("~/Downloads")  # ダウンロードディレクトリのパス
-            new_location = os.path.join(download_dir, download_filename)
-            os.rename(download_filename, new_location)
-
-            st.success(f"データが '{download_filename}' としてエクセルファイルにエクスポートされ、ダウンロードディレクトリに保存されました。")
-        else:
-            st.warning("ダウンロードファイル名を入力してください。")
-    else:
-        st.warning("データが読み込まれていません。")
